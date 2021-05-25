7bbd4a3357a708a9c5b7190f4d990ee52a247084
Leonid <lion6149@mail.ru>
2021-05-26 00:38:59 +0300

    Refactored functions, moved to Utils.kt

--- app/src/main/java/com/example/inspboard/activities/EditProfileActivity.kt
+++ app/src/main/java/com/example/inspboard/activities/EditProfileActivity.kt
@@ -1,129 +1,81 @@
 package com.example.inspboard.activities
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import com.bumptech.glide.Glide
 import com.example.inspboard.R
 import com.example.inspboard.models.User
 import com.example.inspboard.views.PasswordDialog
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.*
 import com.google.firebase.database.*
-import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_edit_profile.*
-import java.io.File
-import java.text.SimpleDateFormat
 import java.util.*
+
+
 
 
 class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
     private val TAG = "EditProfileActivity"
-    private val REQUEST_IMAGE_CAPTURE: Int = 0
-    private lateinit var database: DatabaseReference
-    private lateinit var mStorage: StorageReference
 
     private lateinit var mOldEmail: String
     private lateinit var mNewEmail: String
     private lateinit var mNewUser: User
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var mDataBase: DatabaseReference
-    private  var simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US)
-    private lateinit var mImageUri: Uri
+
+    private lateinit var mCamera: Camera
+    private lateinit var mFirebaseHelper: FirebaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         Log.d(TAG, "onCreate")
 
-        mAuth = FirebaseAuth.getInstance()
-        mDataBase = getDatabaseReference()
-        mStorage = getStorageReference()
+        mCamera = Camera(this)
+        mFirebaseHelper = FirebaseHelper(this)
 
         enableButtonIfAllTextsNonEmpty(button_ok, mail_edit, name_edit)
         button_back.setOnClickListener { finish() }
         button_ok.setOnClickListener { updateUser() }
-        image_view_avatar.setOnClickListener { editUserAvatar() }
+        image_view_avatar.setOnClickListener { mCamera.editUserAvatar() }
 
         val auth = FirebaseAuth.getInstance()
         mOldEmail = auth.currentUser!!.email.toString()
         mail_edit.setText(mOldEmail)
-        database = getDatabaseReference()
-        database.child("users").child(auth.currentUser!!.uid).addListenerForSingleValueEvent(
-            ValueEventListenerAdapter {
-                Log.d(TAG, "user info request ok $it")
-                val user = it.getValue(User::class.java)
-                name_edit.setText(user!!.username, TextView.BufferType.EDITABLE)
-                Glide.with(this).load(user.photo).into(image_view_avatar)
-            })
-    }
 
-    private fun editUserAvatar() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(packageManager) == null) {
-            showToast("Can't get the camera")
-            return
+        mFirebaseHelper.getCurrentUserData { user ->
+            name_edit.setText(user.username, TextView.BufferType.EDITABLE)
+            image_view_avatar.setUserPhoto(user.photo)
         }
-        val imageFile = createImageFile()
-        mImageUri = FileProvider.getUriForFile(
-            this,
-            "com.example.inspboard.fileprovider",
-            imageFile
-        )
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri)
-        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-    }
-
-    private fun createImageFile(): File {
-        // Create an image file name
-        // Create an image file name
-        val storageDir  = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${simpleDateFormat.format(Date())}_",
-            ".jpg",
-            storageDir
-        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode != REQUEST_IMAGE_CAPTURE || resultCode != RESULT_OK)
+        if (requestCode != mCamera.REQUEST_CODE || resultCode != RESULT_OK)
             return
-        val uid = mAuth.currentUser!!.uid
-        val storageRef = mStorage.child("users/$uid/photo")
-        storageRef.putFile(mImageUri).addOnCompleteListener {
-            if (!it.isSuccessful) {
-                showToast(it.exception!!.message!!)
-                return@addOnCompleteListener
-            }
-            storageRef.downloadUrl.addOnCompleteListener { downloadUrlTask ->
-                if (!downloadUrlTask.isSuccessful) {
-                    showToast(downloadUrlTask.exception!!.message!!)
-                    return@addOnCompleteListener
-                }
-                val imageUrl = downloadUrlTask.result.toString()
-                Log.d(TAG, "onActivityResult: $imageUrl")
-                mDataBase.child("users/$uid/photo").setValue(imageUrl).addOnCompleteListener {
-                    if (!it.isSuccessful) {
-                        showToast(it.exception!!.message!!)
-                        return@addOnCompleteListener
+        mFirebaseHelper.apply {
+            storeUserImage(mCamera.imageUri!!) {
+                downloadStoredUserImageUrl { imageUrl ->
+                    updateCurrentUserPhoto(imageUrl) {
+                        image_view_avatar.setUserPhoto(imageUrl)
+                        showToast("Image saved!")
                     }
-                    showToast("Image saved!")
-                    Glide.with(this).load(imageUrl).into(image_view_avatar)
                 }
             }
         }
     }
 
+    private fun updateUserInDb() {
+        val updatesMap = mutableMapOf<String, Any>()
+        updatesMap["username"] = mNewUser.username
+        mFirebaseHelper.updateCurrentUserData(updatesMap) {
+            showToast("User updated")
+            finish()
+        }
+    }
+
     private fun updateUser() {
+        // todo: refactor later
         mNewEmail = mail_edit.text.toString()
         if (mNewEmail.isEmpty()) {
             showToast("Please enter mail")
@@ -140,10 +92,10 @@ class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
         showToast("Updating user...")
 
         if (mNewEmail == mOldEmail) {
-            updateUserInDb(mNewUser)
-            return
+            updateUserInDb()
+        } else {
+            PasswordDialog().show(supportFragmentManager, "password_dialog")
         }
-        PasswordDialog().show(supportFragmentManager, "password_dialog")
     }
 
     override fun onPasswordConfirm(password: String) {
@@ -151,62 +103,17 @@ class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
             showToast("Password is empty")
             return
         }
-        val user = mAuth.currentUser!!
+
         val credential = EmailAuthProvider.getCredential(mOldEmail, password)
-        user.reauthenticate(credential) {
-            user.updateEmail(mNewEmail) {
-                updateUserInDb(mNewUser)
+        mFirebaseHelper.reauthenticate(credential) {
+            mFirebaseHelper.updateEmail(mNewEmail) {
+                updateUserInDb()
             }
-        }
-    }
-    private fun updateUserInDb(user: User) {
-        val updatesMap = mutableMapOf<String, Any>()
-        updatesMap["username"] = user.username
-        database.updateUser(mAuth.currentUser!!.uid, updatesMap) {
-            showToast("User saved")
-            finish()
         }
     }
 
     private fun isUserOk(user: User): String? = when {
-            user.username.isEmpty() -> "Please enter name"
-            else -> null
-        }
-
-    private fun DatabaseReference.updateUser(
-        uid: String,
-        update: Map<String, Any>,
-        onSuccess: () -> Unit
-    ) {
-        child("users")
-            .child(uid)
-            .updateChildren(update)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    onSuccess()
-                    return@addOnCompleteListener
-                }
-                showToast(it.exception!!.message!!)
-            }
-    }
-
-    private fun FirebaseUser.updateEmail(email: String, onSuccess: () -> Unit) {
-        updateEmail(email).addOnCompleteListener {
-            if (it.isSuccessful) {
-                onSuccess()
-                return@addOnCompleteListener
-            }
-            showToast(it.exception!!.message!!)
-        }
-    }
-
-    private fun FirebaseUser.reauthenticate(credential: AuthCredential, onSuccess: () -> Unit) {
-        reauthenticate(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                onSuccess()
-                return@addOnCompleteListener
-            }
-            showToast(it.exception!!.message!!)
-        }
+        user.username.isEmpty() -> "Please enter name"
+        else -> null
     }
 }
--- app/src/main/java/com/example/inspboard/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/inspboard/activities/RegisterActivity.kt
@@ -1,45 +1,29 @@
 package com.example.inspboard.activities
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.Fragment
 import com.example.inspboard.R
 import com.example.inspboard.models.User
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_register_email.*
-import kotlinx.android.synthetic.main.fragment_register_email.edit_text_mail
 import kotlinx.android.synthetic.main.fragment_register_namepass.*
-import kotlinx.android.synthetic.main.fragment_register_namepass.edit_text_password
 
 class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFragment.Listener {
     private val TAG = "RegisterActivity"
     private lateinit var mEmail: String
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var mDataBase: DatabaseReference
+    private lateinit var mFirebaseHelper: FirebaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        mAuth = FirebaseAuth.getInstance()
-        mDataBase = getDatabaseReference()
+        mFirebaseHelper = FirebaseHelper(this)
 
         if (savedInstanceState == null)
-            supportFragmentManager.beginTransaction().add(R.id.frame_layout, EmailFragment())
+            supportFragmentManager.beginTransaction()
+                .add(R.id.frame_layout, EmailFragment())
                 .commit()
     }
 
@@ -48,7 +32,7 @@ class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFr
             showToast("Please enter email")
             return
         }
-        mAuth.fetchSignInMethodsForEmail(email) {
+        mFirebaseHelper.verifyEmailIsUnique(email) {
             mEmail = email
             supportFragmentManager.beginTransaction()
                 .replace(R.id.frame_layout, NamePassFragment())
@@ -67,55 +51,14 @@ class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFr
             showToast("Please enter mail")
             supportFragmentManager.popBackStack()
         }
-        mAuth.createUserWithEmailAndPassword(mEmail, password) {
+        mFirebaseHelper.createUserWithEmailAndPassword(mEmail, password) {
             val user = User(name)
-            mDataBase.createUser(it.user!!.uid, user) {
+            mFirebaseHelper.createUser(it.user!!.uid, user) {
                 showToast("Registered successfully!")
                 startFeedActivity()
             }
         }
 
-    }
-
-    private fun FirebaseAuth.fetchSignInMethodsForEmail(email: String, onSuccess: () -> Unit) {
-        mAuth.fetchSignInMethodsForEmail(email).addOnCompleteListener {
-            if (!it.isSuccessful) {
-                showToast(it.exception!!.message!!)
-                return@addOnCompleteListener
-            } else if (it.result!!.signInMethods?.isEmpty() == false) {
-                showToast("This email already exists")
-                return@addOnCompleteListener
-            } else {
-                onSuccess()
-            }
-        }
-    }
-
-    private fun FirebaseAuth.createUserWithEmailAndPassword(email: String, password: String, onSuccess: (AuthResult) -> Unit) {
-        createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-            if (!it.isSuccessful) {
-                unknownRegisterError("Can't register user with such credentials", it)
-                return@addOnCompleteListener
-            }
-            onSuccess(it.result!!)
-        }
-    }
-
-    private fun DatabaseReference.createUser(uid: String, user: User, onSuccess: () -> Unit) {
-        child("users")
-            .child(uid).setValue(user)
-            .addOnCompleteListener {
-                if (!it.isSuccessful) {
-                    unknownRegisterError("Can't add user to database", it)
-                    return@addOnCompleteListener
-                }
-                onSuccess()
-        }
-    }
-
-    private fun unknownRegisterError(text: String, it: Task<out Any>) {
-        showToast(text)
-        Log.e(TAG, "unknownRegisterError: ", it.exception)
     }
 
     private fun startFeedActivity() {
